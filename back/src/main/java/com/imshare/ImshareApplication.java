d6a9a6420be24f44205c619a5aa9cd5d9523b8d4
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-05-01 20:32:05 -0300

    Start the RESTful implementation

---
+++ back/src/main/java/com/imshare/ImshareApplication.java
@@ -0,0 +1,13 @@
+package com.imshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImshareApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/imshare/ImshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
